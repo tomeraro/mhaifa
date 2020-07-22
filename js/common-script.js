4a13e524608c38904ce5dcccf059427f207e5df4
tomeraro <tomer_aronovsky@hotmail.com>
2020-07-23 00:44:42 +0300

    mhaifa

---
+++ js/common-script.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+  jwplayer('menu-player').setup({
+    'image': './assets/images/img_video.jpg',
+    'stretching': 'fill',
+    'file': 'http://www.youtube.com/v/nLky7bv3cQs',
+  });
+
+
+  // ***** HOME PAGE ****//
+  $('.owl-carousel.news-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    dots: true,
+    items: 1,
+  });
+
+  $('.owl-carousel.next-games-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    dots: false,
+    responsive: {
+            0:{
+              items: 1,
+            },
+            768:{
+              items: 2,
+              margin: 20,
+            },
+            992: {
+              items: 1
+            },
+        }
+  });
+
+  $('.owl-carousel.winnings-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    dots: false,
+    responsive: {
+            0:{
+              items:1,
+              nav: true,
+            },
+            576: {
+              items: 2,
+              nav: true,
+            },
+            768:{
+                items:2,
+                nav: true,
+            },
+            991:{
+                items:3,
+                nav: true,
+            },
+            1200:{
+                items:5
+            },
+        }
+  });
+
+  $('.owl-carousel.store-carousel').owlCarousel({
+  rtl: true,
+  loop: true,
+  dots: false,
+  responsive:{
+          0:{
+            items: 1,
+            nav: true,
+          },
+          768:{
+            items: 2,
+            nav: true,
+          },
+          991: {
+            items:3,
+            nav: true,
+            margin: 20,
+          },
+          1200:{
+            items: 4,
+            margin: 20,
+          },
+          1440:{
+            items: 4,
+            margin: 60,
+          }
+      }
+  });
+
+  // **** ARTICLE ****//
+  $('.owl-carousel.article-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    nav: true,
+    margin: 30,
+  });
+});  
